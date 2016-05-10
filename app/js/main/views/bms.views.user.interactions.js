ce8d271e4d475832194c7832f41c50b64236381c
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2016-05-10 13:59:52 +0200

    added tests for user interactions logs

--- app/js/main/views/bms.views.user.interactions.js
+++ app/js/main/views/bms.views.user.interactions.js
@@ -23,12 +23,15 @@ define([
         if (items) return items.slice().reverse();
       };
     })
-    .directive('bmsUserInteraction', ['ws', 'probWsService', 'bmsModalService', 'bmsSessionService',
-      function(ws, probWsService, bmsModalService, bmsSessionService) {
+    .directive('bmsUserInteraction', ['probWsService', 'bmsModalService', 'bmsSessionService',
+      function(probWsService, bmsModalService, bmsSessionService) {
         'use strict';
 
         return {
-
+          replace: false,
+          scope: {
+            sessionId: '@bmsSessionId'
+          },
           template: '<table class="table table-striped table-condensed">' +
             '<tr><th>Transition</th><th>Executor</th></tr>' +
             '<tr ng-repeat="trans in transitions | reverse" ng-click="gotoTraceIndex(trans)" class="userInteractionsView-item userInteractionsView-item-{{trans.group}}">' +
