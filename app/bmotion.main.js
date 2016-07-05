8fbfd22b11ded53f8a1da1aa0e2cfe5126588f83
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2016-07-05 14:34:12 +0200

    added version to title

--- app/bmotion.main.js
+++ app/bmotion.main.js
@@ -460,7 +460,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
-    title: 'BMotionWeb',
+    title: 'BMotionWeb v0.3.0-SNAPSHOT',
     icon: __dirname + '/resources/icons/bmsicon16x16.png'
   });
   mainWindow.loadUrl('file://' + __dirname + '/index.html#/startServer');
